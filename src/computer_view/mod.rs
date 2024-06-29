f887263f96451bc8c1949c6be14e437fd0a1ca6a
Tommie Nygren <tommie.nygren@gmail.com>
2024-06-29 12:28:50 +0200

    Adds dummy methods for creating and updating ascii ui.

--- src/computer_view/mod.rs
+++ src/computer_view/mod.rs
@@ -1,3 +1,11 @@
+use bevy::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use crate::beats::data::{FactsOfTheWorld, FactUpdated, RuleUpdated, StoryBeatFinished, StoryEngine};
+use crate::beats::systems::{button_system, fact_event_system, fact_update_event_broadcaster, rule_event_system, setup_stories, story_beat_effect_applier, story_evaluator};
+use crate::GameState;
+use crate::ui::banner_widget::BannerWidget;
+use crate::ui::fps_widget;
+use crate::ui::fps_widget::FpsWidget;
 
 pub struct ComputerViewPlugin;
 
@@ -5,9 +13,34 @@ pub struct ComputerViewPlugin;
 // Actions can then be used as a resource in other systems to act on the player input.
 impl Plugin for ComputerViewPlugin {
     fn build(&self, app: &mut App) {
-        // app.init_resource::<Actions>().add_systems(
-        //     Update,
-        //     set_movement_actions.run_if(in_state(GameState::Playing)),
-        // );
+        app.insert_resource(FactsOfTheWorld::new())
+            .add_systems(
+                OnEnter(GameState::ComputerView),
+                (setup_computer_view),
+            )
+            .add_systems(
+                OnExit(GameState::ComputerView),
+                (tear_down_computer_view),
+            )
+            .add_systems(
+                Update,
+                (
+                    update_computer_view
+                ).run_if(in_state(GameState::ComputerView)),
+            )
+        ;
     }
+}
+
+fn setup_computer_view() {
+
+}
+
+
+fn tear_down_computer_view() {
+
+}
+
+fn update_computer_view() {
+
 }
